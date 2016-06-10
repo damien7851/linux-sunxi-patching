fd1eb78f48f59de5908e9aecb7e7a92dbf0e0d9a
Damien Pageot <damien.pageot78@gmail.com>
2016-06-10 13:39:23 +0200

    remove CC

--- ci/compile.sh
+++ ci/compile.sh
@@ -1,8 +1,8 @@
 export ARCH=arm
-export CC=arm-linux-gnueabihf-gcc-4.7
+#export CC=arm-linux-gnueabihf-gcc-4.7
 export CROSS_COMPILE=arm-linux-gnueabihf-
 cd linux-sunxi
 
 cp ../cubietruck_defconfig .config
-make -j`getconf _NPROCESSORS_ONLN` CC=arm-linux-gnueabihf-gcc-4.7 uImage modules | grep -E 'function | Image | Created |warning | error'
+make -j`getconf _NPROCESSORS_ONLN` uImage modules | grep -E 'function | Image | Created |warning | error'
 cd ..
